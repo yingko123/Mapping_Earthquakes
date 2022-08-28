382177f2e8186d51cf4ead6f82b2cf7f2947343a
yingko123 <yingko123@yahoo.com>
2022-08-28 00:35:09 -0500

    update

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -34,8 +34,25 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/yingko123/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data1) {
+        console.log(data1);
+      // Creating a GeoJSON layer with the retrieved data.
+      L.geoJSON(data1,{
+        color: "#ffffa1",
+        weight: 2,
+        onEachFeature: function(feature, layer){
+                layer.bindPopup("<h3> Airline: " + feature.properties.airline +
+                "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+        }
+      }).addTo(map);
+    });
+
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/yingko123/Mapping_Earthquakes/main/majorAirports.json"
+let airportData = "https://raw.githubusercontent.com/yingko123/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data
 d3.json(airportData).then(function(data){
